12fff12a14a7eabf06facd732b1b69515ba785fc
h-tachikawa <tachikawa@feedforce.jp>
2022-02-15 00:17:08 +0900

    chore: implement ui example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
 import '../styles/globals.css';
-import { ChakraProvider } from "@chakra-ui/react";
+import { Avatar, Box, Button, ChakraProvider, Flex, Heading, Spacer } from "@chakra-ui/react";
 import type { AppProps } from 'next/app';
+
+const GlobalNavigation = () => (
+    <Flex h="50px" backgroundColor={'gray.700'} align={'center'}>
+      <Box p={'2'}>
+        <Heading size={'md'} color={'white'}>Realtime YouTube Live Analyzer</Heading>
+      </Box>
+      <Spacer />
+      <Box p={'2'}>
+        <Avatar size={'sm'} bg={'teal.400'} />
+      </Box>
+    </Flex>
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <ChakraProvider>
+        <GlobalNavigation />
         <Component {...pageProps} />
       </ChakraProvider>
   );
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,4 +1,30 @@
 import useSWR from 'swr';
+import { Box, Button, Heading, Stack, Text } from "@chakra-ui/react";
+
+function Feature({ title, desc, ...rest }: { title: string; desc: string }) {
+  return (
+      <Box p={5} shadow='md' borderWidth='1px' {...rest}>
+        <Heading fontSize='xl'>{title}</Heading>
+        <Text mt={4}>{desc}</Text>
+      </Box>
+  );
+}
+
+function StackEx() {
+  return (
+      <Stack spacing={8}>
+        <Feature
+            title='Plan Money'
+            desc='The future can be even brighter but a goal without a plan is just a wish'
+        />
+        <Feature
+            title='Save Money'
+            desc='You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings on your own terms with our completely automated process'
+        />
+      </Stack>
+  );
+}
+
 
 // @ts-ignore
 const fetcher = (...args: any[]) => fetch(...args).then((res) => res.json());
@@ -11,10 +37,7 @@ function Profile() {
   if (!data) return <div>Loading...</div>;
 
   return (
-      <div>
-        <h1>{data.name}</h1>
-        <p>{data.website}</p>
-      </div>
+      <StackEx />
   );
 }
 
